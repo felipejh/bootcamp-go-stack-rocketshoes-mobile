847dfd52c48cb5d8effbd92cc5e29dcfce04a621
felipe.hoffmann <felipe.hoffmann@gmail.com>
2020-03-03 23:11:10 -0300

    Estados do carrinho de compras

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { useSelector, useDispatch } from 'react-redux';
 import colors from '../../styles/colors';
+import { formatPrice } from '../../util/format';
+import * as CartActions from '../../store/modules/cart/actions';
 import {
   Container,
   Products,
@@ -20,90 +23,101 @@ import {
   TotalAmount,
   Order,
   OrderText,
+  EmptyContainer,
+  EmptyText,
 } from './styles';
 
 export default function Cart() {
+  const products = useSelector(state =>
+    state.cart.map(product => ({
+      ...product,
+      subtotal: formatPrice(product.price * product.amount),
+    }))
+  );
+
+  const total = useSelector(state =>
+    formatPrice(
+      state.cart.reduce((totalSum, product) => {
+        return totalSum + product.price * product.amount;
+      }, 0)
+    )
+  );
+
+  const dispatch = useDispatch();
+
+  function increment(product) {
+    dispatch(CartActions.updateAmountRequest(product.id, product.amount + 1));
+  }
+
+  function decrement(product) {
+    dispatch(CartActions.updateAmountRequest(product.id, product.amount - 1));
+  }
+
+  function remove(product) {
+    dispatch(CartActions.removeFromCart(product.id));
+  }
+
   return (
     <Container>
-      <Products>
-        <Product>
-          <ProductInfo>
-            <ProductImage
-              source={{
-                uri:
-                  'https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ims=326x',
-              }}
-            />
-            <ProductDetails>
-              <ProductTitle>Tênis</ProductTitle>
-              <ProductPrice>R$ 35,00</ProductPrice>
-            </ProductDetails>
-            <ProductDelete>
-              <Icon name="delete-forever" color={colors.primary} size={25} />
-            </ProductDelete>
-          </ProductInfo>
-          <ProductControls>
-            <ProductControlButton>
-              <Icon
-                name="remove-circle-outline"
-                color={colors.primary}
-                size={20}
-              />
-            </ProductControlButton>
-            <ProductAmount>5</ProductAmount>
-            <ProductControlButton>
-              <Icon
-                name="add-circle-outline"
-                color={colors.primary}
-                size={20}
-              />
-            </ProductControlButton>
-            <ProductSubtotal>R$250,00</ProductSubtotal>
-          </ProductControls>
-        </Product>
-        <Product>
-          <ProductInfo>
-            <ProductImage
-              source={{
-                uri:
-                  'https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ims=326x',
-              }}
-            />
-            <ProductDetails>
-              <ProductTitle>Tênis</ProductTitle>
-              <ProductPrice>R$ 35,00</ProductPrice>
-            </ProductDetails>
-            <ProductDelete>
-              <Icon name="delete-forever" color={colors.primary} size={25} />
-            </ProductDelete>
-          </ProductInfo>
-          <ProductControls>
-            <ProductControlButton>
-              <Icon
-                name="remove-circle-outline"
-                color={colors.primary}
-                size={20}
-              />
-            </ProductControlButton>
-            <ProductAmount>5</ProductAmount>
-            <ProductControlButton>
-              <Icon
-                name="add-circle-outline"
-                color={colors.primary}
-                size={20}
-              />
-            </ProductControlButton>
-            <ProductSubtotal>R$250,00</ProductSubtotal>
-          </ProductControls>
-        </Product>
-        <TotalContainer>
-          <TotalText>Total</TotalText>
-          <TotalAmount>R$ 1.500,00</TotalAmount>
-          <Order>
-            <OrderText>FINALIZAR PEDIDO</OrderText>
-          </Order>
-        </TotalContainer>
-      </Products>
+      {products.length ? (
+        <>
+          <Products>
+            {products.map(product => (
+              <Product key={product.id}>
+                <ProductInfo>
+                  <ProductImage
+                    source={{
+                      uri: product.image,
+                    }}
+                  />
+                  <ProductDetails>
+                    <ProductTitle>{product.title}</ProductTitle>
+                    <ProductPrice>{total}</ProductPrice>
+                  </ProductDetails>
+                  <ProductDelete onPress={() => remove(product)}>
+                    <Icon
+                      name="delete-forever"
+                      color={colors.primary}
+                      size={25}
+                    />
+                  </ProductDelete>
+                </ProductInfo>
+                <ProductControls>
+                  <ProductControlButton onPress={() => decrement(product)}>
+                    <Icon
+                      name="remove-circle-outline"
+                      color={colors.primary}
+                      size={20}
+                    />
+                  </ProductControlButton>
+                  <ProductAmount>{product.amount}</ProductAmount>
+                  <ProductControlButton onPress={() => increment(product)}>
+                    <Icon
+                      name="add-circle-outline"
+                      color={colors.primary}
+                      size={20}
+                    />
+                  </ProductControlButton>
+                  <ProductSubtotal>{product.subtotal}</ProductSubtotal>
+                </ProductControls>
+              </Product>
+            ))}
+          </Products>
+
+          <TotalContainer>
+            <TotalText>Total</TotalText>
+            <TotalAmount>{total}</TotalAmount>
+            <Order>
+              <OrderText>FINALIZAR PEDIDO</OrderText>
+            </Order>
+          </TotalContainer>
+        </>
+      ) : (
+        <EmptyContainer>
+          <Icon name="remove-shopping-cart" size={64} color="#eee" />
+          <EmptyText>Seu carrinho está vazio.</EmptyText>
+        </EmptyContainer>
+      )}
     </Container>
   );
 }
